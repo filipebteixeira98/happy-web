5617a57e9164990853fc228ab20baf6b30fe385d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-02 16:50:40 -0300

    chore: created landing page structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 import './styles/global.css';
 
+import logoImg from './images/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Happy</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+        <main>
+          <h1>Bring happiness to the world</h1>
+          <p>Visit orphanages and change the day of many children.</p>
+        </main>
+        <div className="location">
+          <strong>Salvador</strong>
+          <span>Bahia</span>
+        </div>
+        <a href="#app" className="enter-app">
+          {`>`}
+        </a>
+      </div>
     </div>
   );
 }
