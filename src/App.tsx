cf8e09486528c60b000a096eb81bef5419598a4c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-02 19:36:48 -0300

    chore: added landing page to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
+
+import Landing from './pages/Landing';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
-
-import logoImg from './images/logo.svg';
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-        <main>
-          <h1>Bring happiness to the world</h1>
-          <p>Visit orphanages and change the day of many children.</p>
-        </main>
-        <div className="location">
-          <strong>Salvador</strong>
-          <span>Bahia</span>
-        </div>
-        <a href="#app" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
-      </div>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
