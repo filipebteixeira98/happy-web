76592f01b159e17e85b09f13c39b6c171cfdde7f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-03 17:28:00 -0300

    chore: changed geolocation integration service to mapbox

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -28,9 +28,12 @@ function OrphanagesMap() {
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
-        <TileLayer
+        {/* <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
       </MapContainer>
       <Link to="" className="create-orphanage">
